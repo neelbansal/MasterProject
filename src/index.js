6848efcaec21b83858a32581c83fdfa3304ecc26
neelbansal <62473674+neelbansal@users.noreply.github.com>
2021-05-04 08:26:56 +0530

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,50 +1,13 @@
-const path = require("path");
-const express = require("express");
-const app = express(); 
-const mongoose = require("mongoose");
-
-
-
-mongoose.connect("mongodb://localhost:27017/Doc", { useNewUrlParser: true, useUnifiedTopology: true})
-.then ( () => console.log("Connection successful...."))
-.catch((err) => console.log(err));
-
-const formSchema = {
-  Name: String,
-  Email: String,
-  Age: Number,
- 
-
-}
-
-const Form = mongoose.model("Form", formSchema);
-
-
-
-const staticPath = path.join(__dirname, "../public");
-
-app.use(express.static(staticPath));
-
-
-
-app.get("/", (req, res) => {
-  res.send("login.html");
-});
-
-app.post('/post-feedback', function (req, res) {
-  mongoose.connect.then(function(db) {
-      delete req.body._id; // for safety reasons
-      db.collection('feedbacks').insertOne(req.body);
-  });    
-  res.send('Data received:\n' + JSON.stringify(req.body));
-});
-app.get('/view-feedbacks',  function(req, res) {
-  mongoose.connect.then(function(db) {
-      db.collection('feedbacks').find({}).toArray().then(function(feedbacks) {
-          res.status(200).json(feedbacks);
-      });
-  });
-});
-app.listen(8000, () => {
-  console.log("Listening to port 8000");
-});
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/appointment';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const app = express();
+const port = 8080;
+app.use(express.static(__dirname + './../public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', require('./api/appointment/'));
+app.listen(port, () => console.log(`Express listening on http://localhost:${port}!`));
